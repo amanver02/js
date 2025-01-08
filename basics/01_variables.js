b292834c2965dcadf786694d81125bfa9bb3ab9c
amanver02 <developeraman02@gmail.com>
2025-01-09 01:26:53 +0530

    basics

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 12123
+let accountEmail = "developeraman02@gmail.com"
+var accountPassword = "1212"
+/**Not use var beacuse there is a problem iin scope but in new js we not use var */
+
+accountEmail = "amanver02@gmail.com"
+accountPassword = "9834"
+let accountState;
+console.log(accountID)
+console.table([accountID, accountEmail,accountPassword,accountState])
